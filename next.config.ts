02e15b74564627cee704794120619bc10a7fc790
kanamaru2525 <mfkanamaru@gmail.com>
2025-01-16 04:31:56 +0900

    [clean]module導入によるファイルの更新

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imgfp.hotp.jp',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
