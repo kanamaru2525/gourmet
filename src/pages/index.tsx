c15f82c81559fc60079d6dc15d62d05a14e4172c
kanamaru2525 <mfkanamaru@gmail.com>
2025-01-17 18:58:01 +0900

    [add]範囲選択の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box,} from "@yamada-ui/react";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
@@ -8,6 +9,8 @@ const Home: React.FC = () => {
   const [searchText, setSearchText] = useState(router.query.keyword || "");
   const [latitude, setLatitude] = useState<string | null>(null);
   const [longitude, setLongitude] = useState<string | null>(null);
+  const [range, setRange] = useState("3"); // 初期値を1に設定
+
 
   // Geolocation APIを使用してユーザーの位置情報を取得
   useEffect(() => {
@@ -31,6 +34,11 @@ const Home: React.FC = () => {
     setSearchText(e.target.value);
   };
 
+   // 範囲選択の変更をハンドリング
+   const handleRangeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRange(e.target.value);
+  };
+
   // 検索ボタンがクリックされたときの処理
   const handleSearch = () => {
     let query = "";
@@ -40,22 +48,38 @@ const Home: React.FC = () => {
     if (latitude && longitude) {
       query += `&lat=${encodeURIComponent(latitude)}&lng=${encodeURIComponent(longitude)}`;
     }
+    if (range) {
+      query += `&range=${encodeURIComponent(range)}`;
+    }
     router.push(`/search?${query}`);
   };
 
   return (
     <div>
-      <p>緯度: {latitude}</p>
-      <p>経度: {longitude}</p>
+      <Box bg="primary" color="white">
+        ホットペッパー
+      </Box>
+      <br />
+        <label htmlFor="range">範囲（1～5）:</label>
+          <select id="range" value={range} onChange={handleRangeChange}>
+            <option value="1">半径300m</option>
+            <option value="2">半径500m</option>
+            <option value="3">半径1000m</option>
+            <option value="4">半径2000m</option>
+            <option value="5">半径3000m</option>
+         </select>
+        
       <input
         type="text"
         value={searchText}
         onChange={handleSearchInput}
         placeholder="キーワード"
       />
+      
       <button type="button" onClick={handleSearch}>
         検索する
       </button>
+      
     </div>
   );
 };
